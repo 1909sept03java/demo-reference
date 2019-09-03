ebe43d24ef0b43a6a0fc6ff51c12a240f9e62327
genesisb17 <genesis.bonds@gmail.com>
2019-09-03 15:11:10 -0400

    Hello World

---
+++ command-prompt-java/HelloWorld.java
@@ -0,0 +1,20 @@
+
+// this is a single line comment
+public class HelloWorld {
+
+        /*
+        THIS IS A MULTILINE COMMENT
+
+        In Java (non-web) applications, there must be at least one main method 
+        in order to start the application thread. This main method syntax is 
+        always the same (with very minimal room to change)
+        */
+        public static void main(String[] args){
+
+            //print to console
+            //this println method takes in a string as a parameter. Strings are written between ""
+            System.out.println("Hello World");
+        }
+
+
+}
