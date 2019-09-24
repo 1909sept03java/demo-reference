ee121e059afe6e6864ebd663963fc1f0b6fb4960
erchiggins <emilyrchiggins@gmail.com>
2019-09-24 11:31:52 -0400

    project setup

---
+++ ServletDemo/src/main/java/com/revature/servlet/HelloWorldServlet.java
@@ -0,0 +1,11 @@
+package com.revature.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class HelloWorldServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -4217738150363355737L;
+	
+	
+
+}
