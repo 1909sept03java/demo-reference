38c6f882080d7aadc166adf5c7802cd121a7d31b
erchiggins <emilyrchiggins@gmail.com>
2019-09-24 12:02:00 -0400

    adding login form

--- ServletDemo/src/main/java/com/revature/servlet/HelloWorldServlet.java
+++ ServletDemo/src/main/java/com/revature/servlet/HelloWorldServlet.java
@@ -1,11 +1,21 @@
 package com.revature.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class HelloWorldServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -4217738150363355737L;
 	
-	
+	// method to handle GET request
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// write a message to the response body with PrintWriter
+		resp.getWriter().write("hello world");
+	}
 
 }
---
+++ ServletDemo/src/main/java/com/revature/servlet/LoginServlet.java
@@ -0,0 +1,26 @@
+package com.revature.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 817105812389880890L;
+
+	/*
+	 * Create two methods: - a doGet to handle GET requests for our login page - a
+	 * doPost to handle POST requests with credentials
+	 */
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// serve the login.html page as a response
+		// RequestDispatcher is used to perform a 'forward' 
+		// (pass the request to another resource without the client knowing)
+		req.getRequestDispatcher("Login.html").forward(req, resp);
+	}
+}
