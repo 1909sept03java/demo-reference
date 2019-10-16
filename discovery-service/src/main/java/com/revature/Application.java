f6ec3505b565ac531e1b1c04df59e3caf197bffc
erchiggins <emilyrchiggins@gmail.com>
2019-10-16 14:58:16 -0400

    spring cloud eureka server and boot-demo as client

--- discovery-service/src/main/java/com/revature/Application.java
+++ discovery-service/src/main/java/com/revature/Application.java
@@ -2,14 +2,14 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
-public class BootDemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootDemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- flashcard-gateway/src/main/java/com/revature/FlashcardGatewayApplication.java
+++ flashcard-gateway/src/main/java/com/revature/FlashcardGatewayApplication.java
@@ -2,14 +2,12 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
-public class BootDemoApplication {
+public class FlashcardGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootDemoApplication.class, args);
+		SpringApplication.run(FlashcardGatewayApplication.class, args);
 	}
 
 }
